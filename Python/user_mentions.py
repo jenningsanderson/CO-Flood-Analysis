b4843fc941815cdfb0a587997bccb01a4ee022f3
Jennings Anderson <jennings.anderson@colorado.edu>
2013-12-12 11:13:24 -0700

    thursday morning

--- Python/user_mentions.py
+++ Python/user_mentions.py
@@ -1,30 +1,41 @@
 """Jennings Anderson 2013
 CSCI 5352: Final Project: Boulder Flooding
 
-This module handles the user mentions graph and does computations for that."""
+This module handles the user mentions graph where users tag other users in
+the actual tweet."""
+
 import bf_load as bf
 import proj_funcs as f
 import networkx as nx
 import matplotlib.pyplot as plt
 import datetime
 
-start = datetime.datetime(2013, 9, 13, 5, 00, 00)
-end = datetime.datetime(2013, 9, 13, 6, 00, 00)
+in_array_query = []
 
-query = {	'spec':	{'created_at': {'$gt': start, '$lt': end } },
-			'fields':{	'_id'		:0, 
-						'id'		:1, 
-						'user.screen_name': 1, 
-						'text'		:1,
-						'user.id'	:1, 
-						'entities.user_mentions':1 }	}
+#start = datetime.datetime(2013, 9, 13, 5, 00, 00)
+#end = datetime.datetime(2013, 9, 13, 6, 00, 00)
+
+# query = {	'spec':	{'created_at': {'$gt': start, '$lt': end } },
+# 			'fields':{	'_id'		:0, 
+# 						'id'		:1, 
+# 						'user.screen_name': 1, 
+# 						'text'		:1,
+# 						'user.id'	:1, 
+# 						'entities.user_mentions':1 }	}
 
 # query = {	'spec': {'geo': {'$ne': None }},	#Only geolocated tweets?
 # 			'fields':{	'_id':0, 'id':1, 'user.screen_name': 1, 'text':1,
 # 						'user.id':1, 'entities.user_mentions':1}
 # 		}
 
-def user_mentions_graph(tweets_array):	# Required fields: 'user.id', 'entities.user_mentions'
+query = {	'spec': {'user.screen_name': {'$in': }},	#Only geolocated tweets?
+			'fields':{	'_id':0, 'id':1, 'user.screen_name': 1, 'text':1,
+						'user.id':1, 'entities.user_mentions':1}
+		}
+
+
+
+def user_mentions_graph(tweets_array):
 	'''Creates User Mentions Graph:
 		Nodes: Users
 		Edges: Exist if user mentions user in tweet, weight = frequency'''
@@ -44,8 +55,7 @@ def user_mentions_graph(tweets_array):	# Required fields: 'user.id', 'entities.u
 	 	
 	 	#Iterate through all of the mentioned users in a tweet
 	 	mentions = tweet['entities']['user_mentions']
-	 	for mentioned in mentions:								# Iterate through each user name in tweet
-	 		
+	 	for mentioned in mentions:
 	 		mentioned_user = int(mentioned['id'])
 			if user_names.has_key(mentioned_user) and not mentioned['screen_name'].encode('ascii', 'ignore') in user_names[mentioned_user]:
 	 			user_names[mentioned_user].append(mentioned['screen_name'].encode('ascii', 'ignore'))
@@ -57,7 +67,7 @@ def user_mentions_graph(tweets_array):	# Required fields: 'user.id', 'entities.u
 	 		else:
 	 			g[this_user][mentioned_user]['weight']+=1
 	 	
-	 	if counter % 1000 == 0:							# For status update, can take a while
+	 	if counter % 1000 == 0:			# For status update, can take a while
 	 		print counter,
 
 	in_degree = g.in_degree(weight='weight')
@@ -70,158 +80,102 @@ def user_mentions_graph(tweets_array):	# Required fields: 'user.id', 'entities.u
 	print "----DONE----"	
 	return g
 
+def show_component_histogram(components, bins=None, debug=False):
+	hist_list = []
+	for i in components:
+		hist_list.append(len(i))
+	if debug:
+		print hist_list
+	if bins:
+		plt.hist(hist_list, bins=bins, histtype='stepfilled')
+	else:
+		plt.hist(hist_list, histtype='stepfilled')
+	plt.title("Size of Components Histogram - Geo-Tagged")
+	plt.xlabel("Nodes in Component")
+	plt.xscale('log')
+	plt.ylabel("Number of Components")
+	plt.show()
+
+def plot_degree_vs_reciprocity():
+	x,y,labels = f.print_top_reciprocated_nodes(large_component, count=10, return_graph=True)
+	fig, ax = plt.subplots()
+	ax.scatter(x, y)
+	for i, txt in enumerate(labels):
+		ax.annotate(txt, (x[i], y[i]), rotation=15,va='bottom')
+	plt.title('Out Degree vs. Reciprocity')
+ 	plt.ylabel("Reciprocity per Node")
+ 	plt.xlabel("Out Degree")
+	plt.show()
+
+def print_top_betweenness(component, size=10):
+	bc = nx.betweenness_centrality(component, weight='weight', normalized=True)
+	for node in sorted(bc, key=bc.get, reverse=True)[0:size]:
+		query = {'spec': {'user.id': int(node) }, 'fields':{'_id':0, 'user.screen_name': 1} }
+		this_data = bf.query_mongo_get_list(query, limit=1)
+		print this_data['user']['screen_name'],'&', "{0:.4f}".format(bc[node]), '\\\\'
+	return bc
+
+def find_self_loops(graph):
+	self_loop_weights = {}
+	for self_loop in graph.selfloop_edges():
+		weight = graph[self_loop[0]][self_loop[1]]['weight']
+		if not self_loop_weights.has_key(weight):
+			self_loop_weights[weight] = [self_loop[0]]
+		else:
+			self_loop_weights[weight].append(self_loop[0])
+
+	weights_array = self_loop_weights.keys()
+	weights_array.sort()
+	weights_array.reverse()
+	for weight in weights_array[0:15]:
+		print weight,"&",
+		if len(self_loop_weights[weight])>1:
+			comma=","
+		else:
+			comma=""
+		for j in self_loop_weights[weight]:
+			query = {'spec': {'user.id': int(j) }, 'fields':{'_id':0, 'user.screen_name': 1, 'text':1} }
+			this_data = bf.query_mongo_get_list(query, limit=1)
+			print this_data['user']['screen_name'], comma, 
+		print "\\\\"
+
 if __name__ == '__main__':
-	print 'Start:', start
-	print 'End:', end
 	
-	user_mentions = bf.query_mongo_get_list(query)
-	print "Tweets:", len(user_mentions)
+	user_mentions = bf.query_mongo_get_list(query)	#Query defined above
+	print "Tweets found:", len(user_mentions), "Making Graph..."	
 	
 	umg = user_mentions_graph(user_mentions)
 
-	large_component = nx.weakly_connected_component_subgraphs(umg)[0]
+	print "Number of components:",
+	components = nx.weakly_connected_component_subgraphs(umg)
+	print len(components)
+	large_component = components[0]
 	print "Nodes in Giant Component: ",len(large_component.nodes())
-
-
-	#bc = nx.betweenness_centrality(large_component, weight='weight')
 
 	print f.get_graph_reciprocity(large_component)
 	print f.get_graph_reciprocity(large_component, weighted=False)
 
-	f.print_top_reciprocated_nodes(large_component, count=10)
+	undirected_umg = umg.to_undirected()
 
-	x,y,labels = f.print_top_reciprocated_nodes(large_component, count=10, return_graph=True)
+	f.draw_graph(print_top_betweenness(undirected_umg, 10), 
+		sort=True, reverse=True, style='ro', scale='log', 
+		title="Betweenness Centrality of Geo-Tagged User Mentions", 
+		x_axis="Nodes", y_axis="Betweenness Centrality")
 
-	fig, ax = plt.subplots()
+
+
+	#For Geo-tagged:
+	#show_component_histogram(components, bins=(0,1,2,3,4,5,6,7,10,13,500,600))
 	
-	ax.scatter(x, y)
+	#f.print_top_reciprocated_nodes(large_component, count=10)
 	
-	for i, txt in enumerate(labels):
-		# if len(txt) %2==0:
-		ax.annotate(txt, (x[i], y[i]), rotation=15,va='bottom')
-
-	plt.title('Out Degree vs. Reciprocity')
- 	plt.ylabel("Reciprocity per Node")
- 	plt.xlabel("Out Degree")
-	
-	plt.show()
-	# I want to compare this data to their out_degree...
-
-
-	# sum = 0
-	# for node in umg.nodes():
-	# 	if "," in umg.node[node]['label']:
-	# 		print umg.node[node]['label'], node, 
-	# 		query = {	'spec':	{'user.id':node}, #{'geo': {'$ne': None }},	#Only geolocated tweets?
-	# 		'fields':{	'_id':0, 'id':1, 'user.screen_name': 1, 'text':1,
-	# 					'user.id':1, 'entities.user_mentions':1}
-	# 	}
-	# 		result = len(bf.query_mongo_get_list(query))
-	# 		print result
-	# 		sum+=result
-
-	# print "these tweets: ", sum
-
-	#Compute the betweenness centrality of the User-mentions graph (For all the users)
-	#bc = nx.betweenness_centrality(large_component)
-	#print len(bc.values())
-	#print 'zeroes', bc.values().count(0.0)
-
-	#f.draw_network_plt(large_component, scale=1)
-
-	# for node in sorted(bc, key=bc.get, reverse=True)[0:10]:
-	# 	query = {'spec': {'user.id': int(node) }, 'fields':{'_id':0, 'user.screen_name': 1} }
-	# 	this_data = bf.query_mongo_get_list(query, limit=1)
-	# 	print this_data['user']['screen_name'],'&', "{0:.4f}".format(bc[node]), '\\\\'
-
-	# #f.draw_graph(bc, sort=True, reverse=True, style='ro', scale='log')
-
-	# for node in large_component.nodes():
-	# 	large_component.node[node]['weight'] = int(10000000*bc[node])
-	# 	large_component.node[node]['label'] = user_names[node]
-
-	#f.write_network_gml(large_component, 'testing_degrees')
-
-
-	# self_loop_weights = {}
-	# for self_loop in umg.selfloop_edges():
-	# 	weight = umg[self_loop[0]][self_loop[1]]['weight']
-	# 	if not self_loop_weights.has_key(weight):
-	# 		self_loop_weights[weight] = [self_loop[0]]
-	# 	else:
-	# 		self_loop_weights[weight].append(self_loop[0])
-
-	# weights_array = self_loop_weights.keys()
-	# weights_array.sort()
-	# weights_array.reverse()
-	# for weight in weights_array[0:15]:
-	# 	print weight,"&",
-	# 	if len(self_loop_weights[weight])>1:
-	# 		comma=","
-	# 	else:
-	# 		comma=""
-	# 	for j in self_loop_weights[weight]:
-	# 		query = {'spec': {'user.id': int(j) }, 'fields':{'_id':0, 'user.screen_name': 1, 'text':1} }
-	# 		this_data = bf.query_mongo_get_list(query, limit=1)
-	# 		print this_data['user']['screen_name'], comma, 
-	# 	print "\\\\"
-
-
-	# degrees = nx.degree(umg)
-
-	# pruned_umg = umg.copy()
-
-	# for node in pruned_umg.nodes():
-	# 	if degrees[node] < 500:
-	# 		pruned_umg.remove_node(node)
-
-	# print len(pruned_umg.nodes())
-	# avg_reciprocity = 0.0
-	# counter = 0
-	# for node in umg.nodes():
-	# 	counter+=1
-	# 	avg_reciprocity += f.get_reciprocity_of_node(umg, node)
-	# 	if counter % 1000==0:
-	# 		print counter,
-
-	# print avg_reciprocity / (len(umg.nodes()))
-
-	#components = nx.weakly_connected_component_subgraphs(umg)
-
-	# hist_list = []
-	# for i in components:
-	# 	hist_list.append(len(i))
-
-	# print hist_list
-
-
-	# plt.hist(hist_list, bins=(0,1,2,3,4,5,6,7,10,13,500,600), histtype='stepfilled')
-	# plt.title("Size of Component Histogram")
-	# plt.xlabel("Size of Component")
-	# plt.xscale('log')
-	# plt.ylabel("Number of Components")
-	# plt.show()
-
-	# counter=0
-	# for i in components[0:3]:
-	# 	print len(i.nodes())
-	# 	counter+=1
-	# 	f.write_network_gml(i, 'umg_subgraph_'+str(counter))
-	 
-
-	#Important - find components:
-
-	#f.write_network_gml(pruned_umg, 'umg_pruned')
-
-	#reciprocity_by_degree(umg, 'out', 15)
-	#What does it look like?
-	#nx.draw(umg)
-	#plt.show()
-
+	#f.write_network_gml(umg, 'geo-tagged-user-mentiones-graph')
 	# bc = nx.betweenness_centrality(umg, normalized=True)
-	# dc = nx.degree_centrality(umg)
-	# print len((umg.nodes()))
-	# draw_graph(bc, sort=True, style="ro" )
-	# draw_graph(dc, sort=True, style="go", y_scale='log')
+	#draw_graph(bc, sort=True, style="ro" )
+
+
+	# This is not that interesting, but it's here:
+	#dc = nx.degree_centrality(large_component)
+	#f.draw_graph(dc, sort=True, style="r-", reverse=True, y_axis="Degree Centrality", x_axis="Number of Nodes")
 
 
